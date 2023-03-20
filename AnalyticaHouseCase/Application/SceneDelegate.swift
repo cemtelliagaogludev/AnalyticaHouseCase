8bd82d39df923c2e72263d72d1fe52418be8558a
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-03-20 23:57:56 +0300

    create navigationController as rootViewController

--- AnalyticaHouseCase/Application/SceneDelegate.swift
+++ AnalyticaHouseCase/Application/SceneDelegate.swift
@@ -19,7 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = HomeConfigurator.createModule()
+            let rootViewController = UINavigationController(rootViewController: HomeConfigurator.createModule())
+            window.rootViewController = rootViewController
             self.window = window
             window.makeKeyAndVisible()
         }
