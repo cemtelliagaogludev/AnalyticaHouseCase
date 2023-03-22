96bb410babb72d2c7530be31749bc2c8ebd4b4de
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-03-22 21:26:42 +0300

    add showAlert function

--- AnalyticaHouseCase/Modules/Home/HomeRouter.swift
+++ AnalyticaHouseCase/Modules/Home/HomeRouter.swift
@@ -5,7 +5,7 @@
 //  Created by admin on 20.03.2023.
 //
 
-import Foundation
+import UIKit
 
 class HomeRouter: HomePresenterToRouter{
     var viewController: HomeViewController?
@@ -14,5 +14,13 @@ class HomeRouter: HomePresenterToRouter{
         let viewController = DetailsConfigurator.createModule()
         viewController.pokemonViewModel = viewModel
         self.viewController?.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    func showAlert(_ errorMessage: String) {
+        let alert = UIAlertController(title: "Error", message: errorMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .cancel))
+        DispatchQueue.main.async {
+            self.viewController?.present(alert, animated: true)
+        }
     }
 }
